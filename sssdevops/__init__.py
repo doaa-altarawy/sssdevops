038ae2c4e484d5c9cad7e43d9d687247bb5a3037
doaa-altarawy <doaa.altarawy@gmail.com>
2018-07-09 19:18:57 -0400

    Update package init

--- sssdevops/__init__.py
+++ sssdevops/__init__.py
@@ -2,8 +2,9 @@
 Main init file for the sssdevop package
 """
 
-# from .sssdevops import *
+from sssdevops import *
 from .list_tools import mean
+from .more_list_tools import split
 
 from ._version import get_versions
 __version__ = get_versions()['version']
