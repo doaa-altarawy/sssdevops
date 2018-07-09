da5c3f6e51316adffb5054a1843c7eb8abce69aa
doaa-altarawy <doaa.altarawy@gmail.com>
2018-07-09 15:11:59 -0400

    Split method

--- sssdevops/more_list_tools.py
+++ sssdevops/more_list_tools.py
@@ -3,5 +3,25 @@ More list manipulations
 """
 
 
-def split(num_list, index):
-    pass
+def split(in_list, index):
+    """
+
+    Parameters
+    ----------
+    in_list: list
+    index: int
+
+    Returns
+    -------
+    Two lists, spliting in_list by 'index'
+
+    Examples
+    -------
+    >>> split(['a', 'b', 'c', 'd'], 3)
+    (['a', 'b', 'c'], ['d'])
+    """
+
+    list1 = in_list[:index]
+    list2 = in_list[index:]
+
+    return list1, list2
