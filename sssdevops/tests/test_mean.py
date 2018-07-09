c7d46df12f1c6d30ae532cca3e30c74ad1d4021b
doaa-altarawy <doaa.altarawy@gmail.com>
2018-07-09 19:02:22 -0400

    Add more tests

--- sssdevops/tests/test_mean.py
+++ sssdevops/tests/test_mean.py
@@ -1,5 +1,29 @@
 from sssdevops import mean
+import pytest
 
 
-def test_simple():
-    assert mean([1, 2, 3, 4, 5]) == 3.0
+@pytest.fixture
+def num_list_3():
+    return [1, 2, 3, 4, 5]
+
+# def test_simple():
+#     assert mean([1, 2, 3, 4, 5]) == 3.0
+
+def test_simple(num_list_3):
+    assert mean(num_list_3) == 3.0
+
+
+def test_empty():
+    with pytest.raises(ZeroDivisionError):
+        mean([])
+
+# def test_size_one():
+#     assert mean([5]) == 5
+
+@pytest.mark.parametrize("num_list, expected_mean", [
+    ([1, 2, 3, 4, 5], 3),
+    ([5], 5),
+    ([1, 2], 1.5),
+])
+def test_many(num_list, expected_mean):
+    assert mean(num_list) == expected_mean
--- sssdevops/tests/test_more_list_tools.py
+++ sssdevops/tests/test_more_list_tools.py
@@ -2,11 +2,25 @@
 Tests more lists tool, the split method
 """
 from sssdevops import split
+import pytest
 
 
+# @pytest.mark.skip
+def test_split_edge():
+    list1, list2 = split([1, 2], 0)
+    assert list1 == [] and list2 == [1, 2]
+
+@pytest.mark.my_easy_test
 def test_split_easy():
     arr = [1, 2, 3, 4, 5]
     list1, list2 = split(arr, 3)
 
-    assert list1 == [1, 2, 3]
-    assert list2 == [4, 5]
+    assert list1 == [1, 2, 3] and list2 == [4, 5]
+
+def test_split_error_index():
+    with pytest.raises(TypeError):
+        split([1, 2, 3], 'd')
+
+def test_split_error_list():
+    with pytest.raises(TypeError):
+        split(45, 4)
